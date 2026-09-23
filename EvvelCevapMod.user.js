f5d998b01f6a334a3b98210d7d2658ec43109af5
Rixsonvoid <269825886+RixsonvoidDev@users.noreply.github.com>
2026-09-23 20:59:53 +0300

    Update user script metadata and comments
    Added license information and cleaned up comments.

--- EvvelCevapMod.user.js
+++ EvvelCevapMod.user.js
@@ -3,11 +3,13 @@
 // @namespace    https://github.com/RixsonvoidDev/custom-user-scripts/
 // @version      1.0
 // @author       RixsonvoidDev
-// @description:tr  Evvel Cevap İçin Mod: Gereksiz Tüm elementleri (Yorumlar Dahil) kaldırır ve otomatik yenilemeyi bozar.
+// @description Evvel Cevap İçin Mod: Gereksiz Tüm elementleri (Yorumlar Dahil) kaldırır ve otomatik yenilemeyi bozar.
+// @description:tr Evvel Cevap İçin Mod: Gereksiz Tüm elementleri (Yorumlar Dahil) kaldırır ve otomatik yenilemeyi bozar.
 // @match        *://evvelcevap.com/*
 // @grant        none
+// @license MIT
 // ==/UserScript==
-
+ 
 (function () {
     'use strict';
     const oldSetTimeout = window.setTimeout;
@@ -16,14 +18,14 @@
         if (delay === 300000) {
             clearTimeout(id);
         }
-
+ 
         return id;
     };
-
+ 
     const banner_observer = new MutationObserver((mutations) => {
         for (const mutation of mutations) {
             if (mutation.addedNodes.length) {
-
+ 
                 const banner1 = document.querySelector('.article-tools__banner');
                 const banner2 = document.querySelector('.article-tools__subjects-list');
                 const banner3 = document.querySelector('.mk-reaction-title');
@@ -34,8 +36,8 @@
                 const banner8 = document.querySelector('.promo-banner.promo-banner--article-lead');
                 const banner9 = document.querySelector('.ac-comments-hub');
                 const popup1 = document.querySelector('.push-toast-wrap');
-
-
+ 
+ 
                 if (banner1) banner1.remove();
                 if (banner2) banner2.remove();
                 if (banner3) banner3.remove();
@@ -49,10 +51,10 @@
             }
         }
     });
-
+ 
     banner_observer.observe(document.documentElement, {
         childList: true,
         subtree: true
     });
-
+ 
 })();
